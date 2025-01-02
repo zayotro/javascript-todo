c18e159dafd50cc46d442eb035c532ce9eb0a06b
Jyoti Saini <jyotisaini6181@gmail.com>
2025-01-02 23:15:14 +0530

    feat: create task form

--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
 import "./style.css";
 import "./fonts.css";
 
-console.log("Another");
+const tasks = [];
+
+const taskList = document.getElementById("task-list");
+const createTaskForm = document.getElementById("createTaskForm");
+const noTaskElement = document.getElementById("no-tasks");
+const taskItem = document.getElementById("task-item");
+
+function render() {
+  if (tasks.length === 0) {
+    taskList.innerHTML = noTaskElement.innerHTML;
+    return;
+  }
+}
+
+function onCreateTaskFormSubmit(event) {
+  const data = new FormData(event.target);
+  const title = data.get("taskName");
+
+  const task = {
+    id: "1234",
+    title: title,
+    date: new Date(),
+    complete: false,
+  };
+
+  tasks.push(task);
+  render();
+}
+
+createTaskForm.addEventListener("submit", onCreateTaskFormSubmit);
+
+render();
